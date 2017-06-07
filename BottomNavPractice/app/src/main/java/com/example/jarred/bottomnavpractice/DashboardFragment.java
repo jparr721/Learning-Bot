6f7c3d48d55aa8ca6f5ad6452204de97c21e95fc
Jarred Parr <jparr721@gmail.com>
2017-06-07 18:34:19 -0400

    I am your grandfather

---
+++ BottomNavPractice/app/src/main/java/com/example/jarred/bottomnavpractice/DashboardFragment.java
@@ -0,0 +1,32 @@
+package com.example.jarred.bottomnavpractice;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DashboardFragment extends Fragment {
+    public static DashboardFragment newInstance() {
+        DashboardFragment fragment = new DashboardFragment();
+        return fragment;
+    }
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+}
